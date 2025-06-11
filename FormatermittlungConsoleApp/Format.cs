9f66073ed0d578c19c1daf0b63b2444343c2ae84
aurelio.eissner <aurelio.eissner@DHM-1168>
2025-06-11 09:12:43 +0200

    Previously, all formats were added to the list even though they didn't match. I've fixed this with better decision logic.

--- FormatermittlungConsoleApp/Format.cs
+++ FormatermittlungConsoleApp/Format.cs
@@ -9,17 +9,19 @@ namespace FormatermittlungConsoleApp
     class Format
 
     {
-        public Format(double diffSquareMM, JsonData item)
+        public Format(double diffSquareMM, JsonData item, string ausrichtung)
         {
             Difference = diffSquareMM;
             
             JsonDataItem = item;
 
+            Ausrichtung = ausrichtung;
+
         }
         public double Difference { get; set; }
         public JsonData JsonDataItem { get; set; }
 
-        public string FormatName { get; set; }
+        public string Ausrichtung { get; set; }
 
 
     }
--- FormatermittlungConsoleApp/Logic.cs
+++ FormatermittlungConsoleApp/Logic.cs
@@ -14,59 +14,83 @@ namespace FormatermittlungConsoleApp
 
         public void logicMethod()
         {
+            const double maxHeight = 594.078152d;
+            const double maxWidth = 443.089168d;
             try
             {
-                Console.Write("Bitte geben Sie die gewünschte Höhe ein: ");
+                Console.Write("Bitte geben Sie die gewünschte Höhe in Millimetern ein: ");
                 double userFormatHeight = Convert.ToDouble(Console.ReadLine());
 
-                Console.Write("Bitte geben Sie die gewünschte Breite ein: ");
-                double userFormatWidth = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Bitte geben Sie die gewünschte Breite in Millimetern ein: ");
+                userFormatWidth = Convert.ToDouble(Console.ReadLine());
 
-            }
-            catch (System.FormatException)
-            {
-
-                Console.WriteLine("Ungültige Eingabe! Bitte versuchen Sie es nochmal.");
-                logicMethod();
-            }
-            
-
-            var client = new RestClient("http://odin-probe.dev.faip.io/api/OrderFormat");
-
-            var request = new RestRequest();
-
-            JsonData[] data = client.Get<JsonData[]>(request);
-
-            double diffSquareMM;
-
-
-            List<Format> optimalFormatList = new List<Format>();
-
-
-            foreach (var item in data)
-            {
-
-                if (item.heightInMM - userFormatHeight > 0 && item.widthInMM - userFormatWidth > 0)
+                if (userFormatHeight <= 0 || userFormatWidth <= 0)
                 {
-                    diffSquareMM = (item.heightInMM * item.widthInMM) - (userFormatHeight * userFormatWidth);
-                    Format format = new Format(diffSquareMM, item);
-                    optimalFormatList.Add(format);
+                    Console.WriteLine("Die Höhe und Breite müssen größer als 0 sein. Bitte versuchen Sie es erneut.");
+                    logicMethod();
+                    
+                }
+                else if (userFormatHeight > maxHeight)
+                {
+                    Console.WriteLine($"Die Höhe darf nicht größer als {maxHeight} sein. Bitte versuchen Sie es erneut.");
+                    logicMethod();
+                    
+                }
+                else if (userFormatWidth > maxWidth)
+                {
+                    Console.WriteLine($"Die Breite darf nicht größer als {maxWidth} sein. Bitte versuchen Sie es erneut.");
+                    logicMethod();
+                    
+                }
+
+                var client = new RestClient("http://odin-probe.dev.faip.io/api/OrderFormat");
+                var request = new RestRequest();
+                JsonData[] data = client.Get<JsonData[]>(request);
+
+                double userArea = userFormatHeight * userFormatWidth;
+                List<Format> optimalFormatList = new List<Format>();
+
+                foreach (var item in data)
+                {
+                    
+                    bool fitsLandscape = item.heightInMM >= userFormatHeight && item.widthInMM >= userFormatWidth;
+                    bool fitsPortrait = item.heightInMM >= userFormatWidth && item.widthInMM >= userFormatHeight;
+
+                    if (fitsLandscape)
+                    {
+                        double diffSquareMM = (item.heightInMM * item.widthInMM) - userArea;
+                        if (diffSquareMM >= 0)
+                        {
+                            Format format = new Format(diffSquareMM, item, "Querformat");
+                            optimalFormatList.Add(format);
+                        }
+                    }else if (fitsPortrait)
+                    {
+                        double diffSquareMM = (item.heightInMM * item.widthInMM) - userArea;
+                        if (diffSquareMM >= 0)
+                        {
+                            Format format = new Format(diffSquareMM, item, "Hochformat");
+                            optimalFormatList.Add(format);
+                        }
+                    }
+                }
+
+                
+                optimalFormatList.Sort((x, y) => x.Difference.CompareTo(y.Difference));
+
+                if (optimalFormatList.Count > 0)
+                {
+                    Console.WriteLine($"Vorgeschlagenes Format: {optimalFormatList[0].JsonDataItem.format} (Verschnitt: {optimalFormatList[0].Difference:F2} mm²)");
                 }
                 else
                 {
-                    continue;
+                    Console.WriteLine("Kein passendes Format gefunden.");
                 }
             }
-
-            optimalFormatList.Sort((x, y) => x.Difference.CompareTo(y.Difference));
-
-            if (optimalFormatList.Count > 0)
+            catch (System.FormatException)
             {
-                Console.WriteLine(optimalFormatList[0].JsonDataItem.format);
-            }
-            else
-            {
-                Console.WriteLine("No optimal format found.");
+                Console.WriteLine("Ungültige Eingabe! Bitte versuchen Sie es nochmal.");
+                logicMethod();
             }
         }
     }
